829d78d9239c8b25d8f627ff8d4eaa3e9d8390b0
YangZigang <tianshanxuester@gmail.com>
2013-11-02 21:57:29 +0800

    hello fragment dynamic

---
+++ HelloFragment/HelloFragmentDynamic/src/com/ingphone/hellofragment/FragmentOne.java
@@ -0,0 +1,16 @@
+package com.ingphone.hellofragment;
+
+import com.example.hellofragment.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+		return inflater.inflate(R.layout.activity_fragment_two, container, false);
+	}
+}
---
+++ HelloFragment/HelloFragmentDynamic/src/com/ingphone/hellofragment/HelloFragment.java
@@ -0,0 +1,38 @@
+package com.ingphone.hellofragment;
+
+import com.example.hellofragment.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.Display;
+import android.view.Menu;
+
+import com.ingphone.hellofragment.FragmentOne;
+import com.ingphone.hellofragment.FragmentTwo;
+
+public class HelloFragment extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hello_fragment);
+		Display display = getWindowManager().getDefaultDisplay();
+		if (display.getWidth() > display.getHeight()) {
+			FragmentOne fragmentOne = new FragmentOne();
+			getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragmentOne).commit();
+		}else {
+			FragmentTwo fragmentTwo = new FragmentTwo();
+			getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragmentTwo).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hello, menu);
+		return true;
+	}
+
+}
