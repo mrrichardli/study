39ee2331cfa09ff700c04873297cc58f101020e0
tianshanxuester <tianshanxuester@gmail.com>
2013-11-19 16:29:20 +0800

    HelloGesture

--- HelloGesture/HelloGesture/src/io/github/tianshanxuester/hellogesture/HelloGesture.java
+++ HelloGesture/HelloGesture/src/io/github/tianshanxuester/hellogesture/HelloGesture.java
@@ -8,6 +8,8 @@ import android.view.GestureDetector.OnDoubleTapListener;
 import android.view.GestureDetector.OnGestureListener;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
 public class HelloGesture extends Activity implements OnGestureListener, OnDoubleTapListener {
 
@@ -20,6 +22,13 @@ public class HelloGesture extends Activity implements OnGestureListener, OnDoubl
 		
 		mGestureDetector = new GestureDetectorCompat(this, this);
 		mGestureDetector.setOnDoubleTapListener(this);
+		
+		findViewById(android.R.id.content).setOnTouchListener(new OnTouchListener(){
+
+			@Override
+			public boolean onTouch(View arg0, MotionEvent event) {
+				return mGestureDetector.onTouchEvent(event);
+			}});
 	}
 
 	@Override
@@ -31,55 +40,55 @@ public class HelloGesture extends Activity implements OnGestureListener, OnDoubl
 
 	@Override
 	public boolean onDoubleTap(MotionEvent event) {
-		Log.d("HelloGesture", "onDoubleTap: " + event.toString());
+		Log.i("HelloGesture", "onDoubleTap: " + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onDoubleTapEvent(MotionEvent event) {
-		Log.d("HelloGesture", "onDoubleTapEvent: " + event.toString());
+		Log.i("HelloGesture", "onDoubleTapEvent: " + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onSingleTapConfirmed(MotionEvent event) {
-		Log.d("HelloGesture", "onSingleTapConfirmed: " + event.toString());
+		Log.i("HelloGesture", "onSingleTapConfirmed: " + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onDown(MotionEvent event) {
-		Log.d("HelloGesture", "onDown: " + event.toString());
+		Log.i("HelloGesture", "onDown: " + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX,
 			float velocityY) {
-		Log.d("HelloGesture", "onFling: " + event1.toString() + event2.toString());
+		Log.i("HelloGesture", "onFling: " + event1.toString() + event2.toString());
 		return true;
 	}
 
 	@Override
 	public void onLongPress(MotionEvent event) {
-		Log.d("HelloGesture", "onLongPress: " + event.toString());
+		Log.i("HelloGesture", "onLongPress: " + event.toString());
 	}
 
 	@Override
 	public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
 			float distanceY) {
-		Log.d("HelloGesture", "onScroll: " + event1.toString() + event2.toString());
+		Log.i("HelloGesture", "onScroll: " + event1.toString() + event2.toString());
 		return true;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent event) {
-		Log.d("HelloGesture", "onShowPress: " + event.toString());
+		Log.i("HelloGesture", "onShowPress: " + event.toString());
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent event) {
-		Log.d("HelloGesture", "onSingleTapUp: " + event.toString());
+		Log.i("HelloGesture", "onSingleTapUp: " + event.toString());
 		return true;
 	}
 
