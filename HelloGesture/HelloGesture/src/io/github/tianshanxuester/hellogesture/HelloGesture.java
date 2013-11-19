e947c4a22b64103333b7a5157bf3ed115460b927
tianshanxuester <tianshanxuester@gmail.com>
2013-11-19 17:52:51 +0800

    HelloGesture

--- HelloGesture/HelloGesture/src/io/github/tianshanxuester/hellogesture/HelloGesture.java
+++ HelloGesture/HelloGesture/src/io/github/tianshanxuester/hellogesture/HelloGesture.java
@@ -40,55 +40,55 @@ public class HelloGesture extends Activity implements OnGestureListener, OnDoubl
 
 	@Override
 	public boolean onDoubleTap(MotionEvent event) {
-		Log.i("HelloGesture", "onDoubleTap: " + event.toString());
+		Log.i("HelloGesture", "onDoubleTap: ");// + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onDoubleTapEvent(MotionEvent event) {
-		Log.i("HelloGesture", "onDoubleTapEvent: " + event.toString());
+		Log.i("HelloGesture", "onDoubleTapEvent: ");// + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onSingleTapConfirmed(MotionEvent event) {
-		Log.i("HelloGesture", "onSingleTapConfirmed: " + event.toString());
+		Log.i("HelloGesture", "onSingleTapConfirmed: ");// + event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onDown(MotionEvent event) {
-		Log.i("HelloGesture", "onDown: " + event.toString());
+		Log.i("HelloGesture", "onDown: " );//+ event.toString());
 		return true;
 	}
 
 	@Override
 	public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX,
 			float velocityY) {
-		Log.i("HelloGesture", "onFling: " + event1.toString() + event2.toString());
+		Log.i("HelloGesture", "onFling: ");// + event1.toString() + event2.toString());
 		return true;
 	}
 
 	@Override
 	public void onLongPress(MotionEvent event) {
-		Log.i("HelloGesture", "onLongPress: " + event.toString());
+		Log.i("HelloGesture", "onLongPress: " );//+ event.toString());
 	}
 
 	@Override
 	public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
 			float distanceY) {
-		Log.i("HelloGesture", "onScroll: " + event1.toString() + event2.toString());
+		Log.i("HelloGesture", "onScroll: ");// + event1.toString() + event2.toString());
 		return true;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent event) {
-		Log.i("HelloGesture", "onShowPress: " + event.toString());
+		Log.i("HelloGesture", "onShowPress: ");// + event.toString());
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent event) {
-		Log.i("HelloGesture", "onSingleTapUp: " + event.toString());
+		Log.i("HelloGesture", "onSingleTapUp: ");// + event.toString());
 		return true;
 	}
 
