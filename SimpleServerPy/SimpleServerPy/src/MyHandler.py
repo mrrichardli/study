4261cc32ac5524fff7df357e7ae4e35f1a31a8b8
tianshanxuester <tianshanxuester@gmail.com>
2013-11-21 17:44:57 +0800

    SimpleServerPy

---
+++ SimpleServerPy/SimpleServerPy/src/MyHandler.py
@@ -0,0 +1,17 @@
+'''
+Created on 2013年11月20日
+
+@author: macminiuser
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
