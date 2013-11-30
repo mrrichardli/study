e413b9c15d0c0c34cff58e39197e0a5a296b3945
YangZigang <tianshanxuester@gmail.com>
2013-11-30 22:41:31 +0800

    HelloFragmentArguments

--- HelloFragment/HelloFragmentArguments/src/com/ingphone/hellofragment/HelloFragment.java
+++ HelloFragment/HelloFragmentArguments/src/com/ingphone/hellofragment/HelloFragment.java
@@ -3,7 +3,6 @@ package com.ingphone.hellofragment;
 import com.example.hellofragment.R;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
