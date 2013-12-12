40bec4c0645b10d11e99da3893e9f23595f51545
tianshanxuester <tianshanxuester@gmail.com>
2013-12-12 17:36:16 +0800

    HelloActionBarDone

--- HelloActionBar/HelloActionBarDone/src/io/github/tianshanxuester/helloactionbar/HelloActionBarActivity.java
+++ HelloActionBar/HelloActionBarDone/src/io/github/tianshanxuester/helloactionbar/HelloActionBarActivity.java
@@ -2,7 +2,9 @@ package io.github.tianshanxuester.helloactionbar;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class HelloActionBarActivity extends ActionBarActivity {
 
@@ -17,6 +19,22 @@ public class HelloActionBarActivity extends ActionBarActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.action_add:
+			Log.i("SMSFilter", "action add");
+			break;
+		case R.id.action_settings:
+			Log.i("SMSFilter", "action setting");
+			break;
+		case R.id.action_delete:
+			Log.i("SMSFilter", "action delete");
+			break;
+		}
+		return true;
 	}
 
 }
