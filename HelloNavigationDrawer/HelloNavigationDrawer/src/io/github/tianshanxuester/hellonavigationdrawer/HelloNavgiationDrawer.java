aabf72e14676fa579b2e4cfb8a4ba9c0933e0400
tianshanxuester <tianshanxuester@gmail.com>
2013-12-18 18:06:57 +0800

    HelloNavigationDrawer

---
+++ HelloNavigationDrawer/HelloNavigationDrawer/src/io/github/tianshanxuester/hellonavigationdrawer/HelloNavgiationDrawer.java
@@ -0,0 +1,101 @@
+package io.github.tianshanxuester.hellonavigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import io.github.tianshanxuester.hellonavigationdrawer.R;
+
+public class HelloNavgiationDrawer extends ActionBarActivity {
+	
+	private String[] mPlanetTitles = new String[]{
+			"金星",
+			"木星",
+			"水星",
+			"火星",
+			"土星"
+	};
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mDrawerList;
+	private LinearLayout mDrawerContainer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_hello_navgiation_drawer);
+		
+		
+		mDrawerContainer = (LinearLayout) findViewById(R.id.left_drawer);
+		
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getSupportActionBar().setTitle("主界面");
+				supportInvalidateOptionsMenu();
+			}
+			
+			public void onDrawerOpened(View view) {
+				getSupportActionBar().setTitle("菜单选择界面");
+				supportInvalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		mDrawerList = (ListView)findViewById(R.id.listview);
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mPlanetTitles));
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+	}
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerContainer);
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+          return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hello_navgiation_drawer, menu);
+		return true;
+	}
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+		
+	}
+	
+	private void selectItem(int position) {
+		mDrawerList.setItemChecked(position, true);
+		setTitle(mPlanetTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerContainer);
+	}
+
+}
